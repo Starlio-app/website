5d0f32792c4ae6faeaafd3670b0f691cbc273df7
Redume <danilivasenko09@gmail.com>
2024-09-14 21:06:09 +0300

    добавил хоум роут

---
+++ src/routes/index.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import FileResponse
+
+router = APIRouter()
+
+@router.get('/')
+async def main():
+    return FileResponse('./src/web/html/index.html')
