3017d85bcd9387902b4fb27889f2e7fac64953f0
Redume <danilivasenko09@gmail.com>
2024-09-14 21:05:43 +0300

    Сделал роуты для рекламы и подтверждение ссылок

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
+from fastapi import FastAPI, APIRouter
+
+from src.routes import index
+from src.routes import wallpaper
+
+app = FastAPI()
+
+app.include_router(index.router)
+app.include_router(wallpaper.router)
+
+app.mount('/static/', StaticFiles(directory='./src/web/static/'))
+app.mount('/.well-known/', StaticFiles(directory='./.well-known/'))
+
+
+@app.route('/app-ads.txt')
+async def app_ads(req, __):
+    return FileResponse('./app-ads.txt')
+
+@app.exception_handler(404)
+async def not_found(req, __):
+    return FileResponse('./src/web/html/error/404.html')
