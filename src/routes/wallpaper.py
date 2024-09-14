2f505d6e5a3e99cf6f54703d03b19bd434e74793
Redume <danilivasenko09@gmail.com>
2024-09-14 21:06:34 +0300

    сделал шаблоан, поменял мета тэги

---
+++ src/routes/wallpaper.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+import requests
+
+router = APIRouter()
+
+template = Jinja2Templates(directory='./src/web/html')
+
+@router.get('/wallpaper/{day}', response_class=HTMLResponse)
+async def wallpaper(request: Request, day):
+    token = '1gI9G84ZafKDEnrbydviGknReOGiVK9jqrQBE3et'
+    res = requests.get('https://api.nasa.gov/planetary/apod', params={
+        'api_key': '1gI9G84ZafKDEnrbydviGknReOGiVK9jqrQBE3et',
+        'date': day,
+    })
+        
+    print(res.json())
+
+    return template.TemplateResponse(
+        request,
+        '/wallpaper.html',
+        {'info': res.json()}
+    )
