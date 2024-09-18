e4b236158cdefbe65b8f8768f441e3a8a0d5641f
Redume <danilivasenko09@gmail.com>
2024-09-18 21:27:26 +0300

    Сделал редирект 404 ошибки, если нет данных

--- src/routes/wallpaper.py
+++ src/routes/wallpaper.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 
 import requests
@@ -11,11 +11,13 @@ template = Jinja2Templates(directory='./src/web/html')
 
 @router.get('/wallpaper/{day}', response_class=HTMLResponse)
 async def wallpaper(request: Request, day):
-    token = '1gI9G84ZafKDEnrbydviGknReOGiVK9jqrQBE3et'
     res = requests.get('https://api.nasa.gov/planetary/apod', params={
         'api_key': '1gI9G84ZafKDEnrbydviGknReOGiVK9jqrQBE3et',
         'date': day,
     })
+
+    if res.status_code != 200:
+        return FileResponse('./src/web/html/error/404.html')
 
     return template.TemplateResponse(
         request,
