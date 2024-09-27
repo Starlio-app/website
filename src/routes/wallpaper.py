cc8e0333f9da8cfd1b67295cef8c51c3027d2041
Redume <danilivasenko09@gmail.com>
2024-09-27 17:34:59 +0300

    Добавлен новый роут, теперь можно получить сегодняшние обои

--- src/routes/wallpaper.py
+++ src/routes/wallpaper.py
@@ -1,20 +1,34 @@
 from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 import requests
+from datetime import datetime
 
 router = APIRouter()
 
 template = Jinja2Templates(directory='./src/web/html')
 
+def get_wallpaper(date: str):
+    return requests.get('https://api.nasa.gov/planetary/apod', params={
+        'api_key': '1gI9G84ZafKDEnrbydviGknReOGiVK9jqrQBE3et',
+        'date': date,
+    })
+
+@router.get('/wallpaper/today', response_class=HTMLResponse)
+async def today_wallpaper(requests: Request):
+    date = datetime.today().strftime('%Y-%m-%d')
+    res = get_wallpaper(date)
+
+    if res.status_code != 200:
+        return FileResponse('./src/web/html/error/404.html')
+
+    return RedirectResponse(f'/wallpaper/{date}')
+
 
 @router.get('/wallpaper/{day}', response_class=HTMLResponse)
 async def wallpaper(request: Request, day):
-    res = requests.get('https://api.nasa.gov/planetary/apod', params={
-        'api_key': '1gI9G84ZafKDEnrbydviGknReOGiVK9jqrQBE3et',
-        'date': day,
-    })
+    res = get_wallpaper(day)
 
     if res.status_code != 200:
         return FileResponse('./src/web/html/error/404.html')
